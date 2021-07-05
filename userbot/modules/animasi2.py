cad155067d8998d077d73b910a9737deb136fe00
Shin Ryujin <82243933+ferikunn@users.noreply.github.com>
2021-07-06 04:33:53 +0700

    Update and rename feriganss.py to animasi2.py

--- userbot/modules/animasi2.py
+++ userbot/modules/animasi2.py
@@ -216,9 +216,9 @@ async def typewriter(typew):
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(1)
-    await typew.edit("🐧")
+    await typew.edit("❤")
     sleep(2)
-    await typew.edit("**🐧LINUX - USERBOT🐧**\n\n [Tekan Disini](https://github.com/ferikunn/Linux-Userbot)\n Kalian kya Babi...")
+    await typew.edit("**📍Treep-Bot📍**\n\n [Tekan Disini](https://github.com/ferikunn/Treep-Bot)\n Kalian seperti Babi...")
 
 CMD_HELP.update({
     "animasi2":
