350215faaf442b719a45fda6468df3da9b8a6cc0
Shin Ryujin <82243933+ferikunn@users.noreply.github.com>
2021-07-05 18:00:24 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"🐧Linux-Userbot🐧 ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
+    f"📍Treep-Bot📍 ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
 
 
 if len(argv) not in (1, 3, 4):
