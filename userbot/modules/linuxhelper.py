01ff3db811b76272baf301986082c671b97dbe7e
Shin Ryujin <82243933+ferikunn@users.noreply.github.com>
2021-07-05 19:13:24 +0700

    Update linuxhelper.py

--- userbot/modules/linuxhelper.py
+++ userbot/modules/linuxhelper.py
@@ -8,12 +8,12 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 # ============================================
 
 
-@register(outgoing=True, pattern="^.lhelp$")
+@register(outgoing=True, pattern="^.thelp$")
 async def usit(e):
     await e.edit(
         f"**Hai Tuan {DEFAULTUSER} Kalau Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
         "\n[Mastah](t.me/xflicks)"
-        "\n[Repo](https://github.com/ferikunn/Linux-Userbot)"
+        "\n[Repo](https://github.com/ferikunn/Treep-Bot)"
         "\n[Instagram Mastah](Instagram.com/ferikunn)")
 
 
@@ -21,13 +21,13 @@ async def usit(e):
 async def var(m):
     await m.edit(
         f"**Disini Daftar Vars Dari {DEFAULTUSER}:**\n"
-        "\n[DAFTAR VARS](https://raw.githubusercontent.com/ferikunn/Linux-Userbot/Linux-Userbot/varshelper.txt)")
+        "\n[DAFTAR VARS](https://raw.githubusercontent.com/ferikunn/Treep-Bot/Treep-Bot/varshelper.txt)")
 
 
 CMD_HELP.update({
     "linuxhelper":
-    "🐧CMD🐧`.lhelp`\
-\nPenjelasan: Bantuan Untuk saya-Userbot.\
-\n🐧CMD🐧`.vars`\
+    "CMD`.lhelp`\
+\nPenjelasan: Bantuan Untuk 📍Treep-Bot📍.\
+\nCMD`.vars`\
 \nPenjelasan: Untuk Melihat Beberapa Daftar Vars."
 })
