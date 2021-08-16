8b09e562330152d6bb1f01ce63da0944e6673b0e
Abror0110 <86902404+Abror0110@users.noreply.github.com>
2021-08-16 10:50:47 +0700

    Update logo.py

--- userbot/modules/logo.py
+++ userbot/modules/logo.py
@@ -1,106 +1,51 @@
-# Thanks Full To Team Ultroid
-#
-# Geez - Projects <https://github.com/Vckyou/Geez-UserBot/>
-# Ported By Vcky @VckyouuBitch
-# Copyright (c) 2021 Geez - Projects
-#
+# 🍀 © @tofik_dn
+# ⚠️ Do not remove credits
 
-import glob
-import os
-import random
-
-from PIL import Image, ImageDraw, ImageFont
-from telethon.tl.types import InputMessagesFilterPhotos
-
-from userbot.events import register
+import asyncio
+from telethon.errors.rpcerrorlist import YouBlockedUserError
 from userbot import CMD_HELP, ALIVE_NAME
-
-# ================= CONSTANT =================
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
-# ============================================
+from userbot.events import register
 
 
-@register(outgoing=True, pattern="^.logo(?: |$)(.*)")
-async def logo_gen(event):
-    xx = await event.edit("`Processing.....`")
-    name = event.pattern_match.group(1)
-    if not name:
-        await xx.edit("`Give a name too!`")
-    bg_, font_ = "", ""
-    if event.reply_to_msg_id:
-        temp = await event.get_reply_message()
-        if temp.media:
-            if hasattr(temp.media, "document"):
-                if "font" in temp.file.mime_type:
-                    font_ = await temp.download_media()
-                elif (".ttf" in temp.file.name) or (".otf" in temp.file.name):
-                    font_ = await temp.download_media()
-            elif "pic" in mediainfo(temp.media):
-                bg_ = await temp.download_media()
+@register(outgoing=True, pattern=r"^\.logo(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    text = event.pattern_match.group(1)
+    if not text:
+        await event.edit("`Give a name too!`")
     else:
-        pics = []
-        async for i in event.client.iter_messages(
-            "@AnosLogo", filter=InputMessagesFilterPhotos
-        ):
-            pics.append(i)
-        id_ = random.choice(pics)
-        bg_ = await id_.download_media()
-        fpath_ = glob.glob("resources/fonts/*")
-        font_ = random.choice(fpath_)
-    if not bg_:
-        pics = []
-        async for i in event.client.iter_messages(
-            "@AnosLogo", filter=InputMessagesFilterPhotos
-        ):
-            pics.append(i)
-        id_ = random.choice(pics)
-        bg_ = await id_.download_media()
-    if not font_:
-        fpath_ = glob.glob("resources/fonts/*")
-        font_ = random.choice(fpath_)
-    if len(name) <= 8:
-        fnt_size = 150
-        strke = 10
-    elif len(name) >= 9:
-        fnt_size = 50
-        strke = 5
-    else:
-        fnt_size = 130
-        strke = 20
-    img = Image.open(bg_)
-    draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype(font_, fnt_size)
-    w, h = draw.textsize(name, font=font)
-    h += int(h * 0.21)
-    image_width, image_height = img.size
-    draw.text(
-        ((image_width - w) / 2, (image_height - h) / 2),
-        name,
-        font=font,
-        fill=(255, 255, 255),
-    )
-    x = (image_width - w) / 2
-    y = (image_height - h) / 2
-    draw.text((x, y), name, font=font, fill="white",
-              stroke_width=strke, stroke_fill="black")
-    flnme = f"rose.png"
-    img.save(flnme, "png")
-    await xx.edit("`Done!`")
-    if os.path.exists(flnme):
+        await event.edit("`Processing`")
+    chat = "@tdtapibot"
+    async with event.client.conversation(chat) as conv:
+        try:
+            msg = await conv.send_message(f"/logo {text}")
+            response = await conv.get_response()
+            logo = await conv.get_response()
+            """ - don't spam notif - """
+            await event.client.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await event.edit(
+                "**Error: Mohon Buka Blokir** @tdtapibot **Dan Coba Lagi!**"
+            )
+            return
+        await asyncio.sleep(0.5)
         await event.client.send_file(
             event.chat_id,
-            file=flnme,
-            caption=f"Logo by @Rorflame",
-            force_document=True,
+            logo,
+            caption=f"Logo by {ALIVE_NAME}"
         )
-        os.remove(flnme)
-        await xx.delete()
-    if os.path.exists(bg_):
-        os.remove(bg_)
-    if os.path.exists(font_):
-        if not font_.startswith("resources/fonts"):
-            os.remove(font_)
+        await event.client.delete_messages(
+            conv.chat_id, [msg.id, response.id, logo.id]
+        )
+        await event.delete()
 
 
-CMD_HELP.update({"logo": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.logo <text>`"
-                 "\n↳ : Hasilkan logo dari Teks atau Balas Ke gambar yang diberikan, untuk menulis teks Anda di atasnya. Atau Balas Ke File Font, Untuk menulis dengan font itu."})
+CMD_HELP.update(
+    {
+        "logo": "**Plugin : **`logo`\
+        \n\n  •  **Syntax :** `.logo` <text>\
+        \n  •  **Function : **Membuat logo dari Teks yang diberikan\
+    "
+    }
+)
